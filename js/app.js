c1f483ea3c82eed30b899e54c97a373fbbbb5b95
VladimirAur <49985521+VladimirAur@users.noreply.github.com>
2019-12-25 16:43:22 +0200

    add jquerry for test

--- js/app.js
+++ js/app.js
@@ -1,69 +1,24 @@
 // // Управление модальными окнами
-// const project = document.getElementById('project');
-// const buttonOpen = document.querySelectorAll('.project_type-caption');
-// const buttonClose = document.querySelectorAll('.popup_content_close');
-// const containerContent = document.querySelector('.wrapper');
 
-// // Klick//////////////////////////////////////////////////
-// buttonOpen.forEach(function(item){
-// 	item.addEventListener('click', function(){
-// 		let popupName = item.getAttribute('data-popup');
-// 		let popup = document.getElementById(popupName);
-// 		popup.classList.toggle('popup-open');
-// 		popup.scrollIntoView();
-// 		containerContent.classList.toggle("wrapper-fixed");
-// 	})  
-// });
-
-// buttonClose.forEach(function(item){
-// 	item.addEventListener('click', function(){
-// 		let popup = item.closest('.popup');
-// 		popup.classList.toggle('popup-open');
-// 		containerContent.classList.toggle("wrapper-fixed");
-// 		project.scrollIntoView();
-
-// 	})
-// });
-// // //////////////////////////////////////////////////////
-
-// touchstart/////////////////////////////////////////////
 $('.project_type-caption').click(function(){
 	var popupName = $(this).attr('data-popup');
-	$(`#${popupName}`).toggleClass('popup-open');	
+	var popup = document.getElementById(popupName);
+	$(`#${popupName}`).toggleClass('popup-open');
+	popup.scrollIntoView();	
 	$('.wrapper').toggleClass("wrapper-fixed");
 });
 
 $('.popup_content_close').click(function(){
 	var popup = $(this).closest('.popup');
+	var project = document.getElementById('project');
 	popup.toggleClass('popup-open');	
-	$('.wrapper').toggleClass("wrapper-fixed");	
+	$('.wrapper').toggleClass("wrapper-fixed");
+	project.scrollIntoView();	
 })
-
-
 ///////////////////////////////////////////////////////
 
 // Управление блоком меню
-// const menuButton = document.getElementById('navToggle');
-// const menu = document.querySelector('.overlay')
-// const menuItem = document.querySelectorAll('.menu_item')
 
-// // Klick//////////////////////////////////////////////
-// menuButton.addEventListener('click', function(){
-// 	menuButton.classList.toggle("active");
-// 	menu.classList.toggle("open");
-// 	containerContent.classList.toggle("wrapper-fixed");
-// });
-
-// menuItem.forEach(function(item){
-// 	item.addEventListener('click', function(){
-// 	menuButton.classList.toggle("active");
-// 	menu.classList.toggle("open");
-// 	containerContent.classList.toggle("wrapper-fixed");	
-// 	})
-// });
-// ////////////////////////////////////////////////////
-
-// touchstart/////////////////////////////////////////////
 $("#navToggle").click(function() {
 	    $(this).toggleClass("active");
 	    $(".overlay").toggleClass("open");
@@ -81,22 +36,7 @@ $(".menu_item").click(function(){
 
 
 // Управление акордеоном для услуг
-// const serviceButton = document.querySelectorAll('.service_type-button');
 
-
-// // Klick////////////////////////////////////////////////
-// serviceButton.forEach(function(item){
-// 	item.addEventListener('click', function(){
-// 		event.preventDefault();
-// 		this.classList.toggle('opend');
-// 		let tableName = item.getAttribute('data-table');
-// 		let table = document.getElementById(tableName);
-// 		table.classList.toggle('table-open');
-// 	})
-// })
-// // /////////////////////////////////////////////////////
-
-// touchstart/////////////////////////////////////////////
 $('.service_type-button').click(function(){
 		event.preventDefault();
 		$(this).toggleClass('opend');
